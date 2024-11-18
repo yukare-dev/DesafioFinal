b8dbed8d56d453675e81345d6a5ae7b64880edd7
yukapx <yukare1998@gmail.com>
2024-11-18 15:40:28 -0300

    feat: header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,18 +2,32 @@ import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 import Inicio from '../../pages/Inicio/Inicio'
 import LivrosDoados from '../../pages/livrosDoados/LivrosDoados'
 import QueroDoar from '../../pages/queroDoar/QueroDoar'
+import logoLivro from '../../assets/logoLivro.png'
+import search from '../../assets/search.png'
+import s from './header.module.scss'
+
 
 export default function Header(){
     return(
         <BrowserRouter>
             <header>
-                <nav>
+                <section className={s.logoHeader}>
+                    <img src={logoLivro} alt="Imagem ilustrativa de uma logo" />
+                    <h1>Livros Vai na Web</h1>
+                </section>
+                <nav className={s.navHeader}>
                     <ul>
-                        <li><Link to='/'>Início</Link></li>
-                        <li><Link to='/LivrosDoados'>Livros Doados</Link></li>
-                        <li><Link to='/QueroDoar'>Quero Doar</Link></li>
+                        <li><Link className={s.link} to='/'>Início</Link></li>
+                        <li><Link className={s.link} to='/LivrosDoados'>Livros Doados</Link></li>
+                        <li><Link className={s.link} to='/QueroDoar'>Quero Doar</Link></li>
                     </ul>
                 </nav>
+                <section className={s.barraDeBusca}>
+                    <input type="search" name="" id="" placeholder='O que você procura?' />
+                    <button>
+                        <img src={search} alt="Imagem de uma lupa branca" />
+                    </button>
+                </section>
             </header>
             <Routes>
                 <Route path='/' element={<Inicio/>}/>
