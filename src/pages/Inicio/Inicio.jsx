76956937a0aeaf9ea7c96f8c34dcb5005b434e0d
yukapx <yukare1998@gmail.com>
2024-11-22 14:56:54 -0300

    add: home

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -1,5 +1,11 @@
+import s from './inicio.module.scss'
+
 export default function Inicio(){
     return(
-        <h2>eu sou o inicio</h2>
+        <main>
+            <section className={s.primeiraSessaoInicio}>
+                <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
+            </section>
+        </main>
     )
 }
