53c351410bd4d24993fe4d28543bd0be99be5e9e
yukare-dev <yukare1998@gmail.com>
2024-11-29 17:50:39 -0300

    add: responsive header

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -5,12 +5,16 @@ export default function LivrosDoados(){
     return(
         <section className={s.donatePage}>
             <h2>Livros Doados</h2>
-            <div className={s.donateBook}>
+            <section className={s.donateBook}>
+                <div className={s.books}>
                 <img src={livroOProtagonista} alt="Imagem da capa do livro O Protagonista" />
-                <p>O protagonista</p>
+                <h3>O protagonista</h3>
+                <div>
                 <p>Susanne Andrade</p>
                 <p>Ficção</p>
-            </div>
+                </div>
+                </div>
+            </section>
         </section>
     )
 }
